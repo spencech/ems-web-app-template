a20a80207451d97ef9aa58b253c1a27b12028095
spencech <spencech@gmail.com>
2022-07-07 14:47:19 -0400

    adding placeholder dirs for pipes/utils

---
+++ src/utils/methods.utils.ts
@@ -0,0 +1,9 @@
+/** 
+ * See https://github.com/spencech/ems-web-app-utils for already included utility methods 
+ * 
+ * Export app specific utility methods as follows:
+ * 
+ * export function example(): string {
+ * 	return "example utility method"
+ * }
+**/
